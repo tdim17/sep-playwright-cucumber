e9fdd98d889f02369a04ebd1b194fdeb929e7de3
TDIM <dmitry.tus@gmail.com>
2024-11-17 23:49:35 -0500

    @sep17 beginning

--- features/payment-plans/ViewPaymentPlanOptionStep2.feature
+++ features/payment-plans/ViewPaymentPlanOptionStep2.feature
@@ -21,4 +21,22 @@ Feature: View payment plan options in Step 2   #! test only
 
     #TODO: Create scenarios that cover all the acceptance criteria
 
-    
+    Background:
+        Given user is on the enrollment page
+        And user is on step two of the enrollment process
+
+    @sep17-1
+    Scenario: verify upfront price block
+        Then there should be only one upfront price
+    #    Then text the first row should be 'Ubfront'
+    #    Then the second row should be '$ <upfont_price> pay once'
+
+    Scenario: verify installment price block
+    #    Then there must be total <num> Payment Plans
+    #    Then there can be <number_of_installments> installments
+    #    Then text of first row sould be '<number_of_installments> Installments' 
+    #    Then text of second row sould be '<$ <monthly_price> per month'
+    #    Then installment plans should be unique 
+
+
+
--- steps/payment-plans/viewPaymentPlanOptionStep2.js
+++ steps/payment-plans/viewPaymentPlanOptionStep2.js
@@ -2,3 +2,90 @@ import { Given, Then, When } from "@cucumber/cucumber";
 import { expect} from "@playwright/test";
 import { paymentPlanPage, page } from "../../globalPagesSetup.js";
 import { productInfo } from "../../utilities/qa-data-reader.js";
+
+
+Then('there should be only one upfront price', async function () {
+
+     const upfrontBlock = paymentPlanPage.UpfrontText.textContent;
+     await expect(upfrontBlock).toHaveCount(1);
+
+    // const spanElements = paymentPlanPage.UpfrontText;
+
+    // const count = await spanElements.count();
+    // let upfrontCount = 0;
+
+    // for (let i = 0; i < count; i++) {
+    //   const text = await spanElements.nth(i).textContent();
+    //   if (text === 'upfront') {
+    //     upfrontCount++;
+    //   }
+    // }
+
+    //expect(upfrontCount).toBe(1);
+
+});
+
+Then('text the first row should be {string}', async function (string) {
+});
+
+Then('the second row should be {string}', async function (string) {
+});
+
+
+
+
+
+Then('there must be total <num> Payment Plans', async function () {
+});
+
+Then('there can be <number_of_installments> installments', async function () {
+});
+
+Then('text of first row sould be {string}', async function (string) {
+});
+
+Then('text of second row sould be {string}', async function (string) {
+});
+
+Then('installment plans should be unique', async function () {
+});
+
+
+test('Verify upfront and installment price blocks', async ({ page }) => {
+    // Navigate to the enrollment page and proceed to step two
+    await page.goto('https://example.com/enrollment');
+    await page.click('button:text("Next")');
+  
+    // Verify upfront price block
+    const upfrontBlock = await page.locator('.upfront-price-block');
+    await expect(upfrontBlock).toHaveCount(1);
+  
+    const upfrontRows = await upfrontBlock.locator('.row');
+    await expect(upfrontRows.nth(0)).toHaveText('Upfront');
+    
+    const upfrontPrice = await upfrontRows.nth(1).textContent();
+    await expect(upfrontRows.nth(1)).toHaveText(new RegExp(`\\$\\s*\\d+(\\.\\d{2})?\\s*pay once`));
+  
+    // Verify installment price block
+    const installmentBlocks = await page.locator('.installment-price-block');
+    const numPaymentPlans = await installmentBlocks.count();
+  
+    for (let i = 0; i < numPaymentPlans; i++) {
+      const block = installmentBlocks.nth(i);
+      const rows = await block.locator('.row');
+  
+      const firstRowText = await rows.nth(0).textContent();
+      const numInstallments = parseInt(firstRowText.split(' ')[0]);
+      await expect(rows.nth(0)).toHaveText(`${numInstallments} Installments`);
+  
+      const secondRowText = await rows.nth(1).textContent();
+      await expect(rows.nth(1)).toHaveText(/\$\s*\d+(\.\d{2})?\s*per month/);
+  
+      // Verify uniqueness of installment plans
+      if (i > 0) {
+        const prevBlockText = await installmentBlocks.nth(i - 1).textContent();
+        await expect(block).not.toHaveText(prevBlockText);
+      }
+    }
+  });
+
