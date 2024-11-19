343c83b7e002d6a1356ff59a8a2904e4a8e8ee82
TDIM <dmitry.tus@gmail.com>
2024-11-19 17:01:25 -0500

    @sep-25 is completely done

--- steps/submitting-payments/errorMessageCardNumber.js
+++ steps/submitting-payments/errorMessageCardNumber.js
@@ -2,3 +2,29 @@ import { Given, Then, When } from "@cucumber/cucumber";
 import { expect} from "@playwright/test";
 import { reviewPaymentPage, page } from "../../globalPagesSetup.js";
 import { productInfo } from "../../utilities/qa-data-reader.js";
+
+let errorMessageElement = "";
+let cardNumberInputElement = "";
+
+When('user sequentially enters a short card number from {int} to {int} characters', async function (int, int2) {
+
+// Definition of locators elements:
+cardNumberInputElement = reviewPaymentPage.cardNumberInput;
+errorMessageElement = reviewPaymentPage.cardNumberErrorMessage;
+
+await cardNumberInputElement.fill("1234");
+await page.click('body');
+});
+
+Then('a message {string} should be thrown immediately', async function (string) {
+const errorIncompleteExpected = string;
+const errorMessageActual = await errorMessageElement.innerText();
+console.log("errorIncompleteExpected : " + errorIncompleteExpected);
+console.log("errorMessageActual : " + errorMessageActual);
+expect(errorMessageActual).toEqual(errorIncompleteExpected);
+});
+
+When('user enters the {string} card number', async function (cardNumberString) {
+    await cardNumberInputElement.fill(cardNumberString);
+    await page.click('body');
+});
