86ee09be2263551450d4952ef948f011a6bb8f43
TDIM <dmitry.tus@gmail.com>
2024-11-29 16:42:07 -0500

    dev up to date

--- pages/PaymentPlanPage.js
+++ pages/PaymentPlanPage.js
@@ -17,7 +17,7 @@ export class PaymentPlanPage extends BasePage {
       "//span[@class='discount-price']/span"
     );
     this.upfrontPaymentFrame = page.locator(
-      "//div[@class='content-panel-item ng-star-inserted']/i[contains(text(), 'excluding fees')]"
+      "(//mat-expansion-panel-header[@role='button'])[1]"
     );
     this.greenBadgeUpfrontDiscount = page.locator(
       "//span[@class='chip-content']"
