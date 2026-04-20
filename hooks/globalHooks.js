f1c7ff364f3f4f10adf25a33d9fe1c2eba3e0fd9
TDIM <dmitry.tus@gmail.com>
2026-04-19 23:03:21 -0400

    Fix: enable headless mode on CI

--- hooks/globalHooks.js
+++ hooks/globalHooks.js
@@ -5,7 +5,7 @@ import fs from 'fs';
 import path from 'path';
 
 const BROWSER_TYPE = "chrome";
-const HEADLESS_MODE = false;
+const HEADLESS_MODE = process.env.CI === 'true' || process.env.CI === true ? true : false;
 const MAXIMIZED_WINDOW = true;
 const SLOW_MOTION_DELAY = 0; // slow mode in milliseconds
 const DEFAULT_TIMEOUT = 20000; // default timeout in milliseconds
